56db8eab52f5dc6baab4b11908958576ec1a3c23
Yuxuan Zhao <me@yuxuanz.com>
2020-01-19 22:14:14 -0700

    update the code

--- httpclient.py
+++ httpclient.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,16 +24,19 @@ import re
 # you may use urllib to encode data appropriately
 import urllib.parse
 
+
 def help():
     print("httpclient.py [GET/POST] [URL]\n")
+
 
 class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
 
+
 class HTTPClient(object):
-    #def get_host_port(self,url):
+    # def get_host_port(self,url):
 
     def connect(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -41,17 +44,41 @@ class HTTPClient(object):
         return None
 
     def get_code(self, data):
-        return None
+        request_line = data.split("\r\n")[0]
+        self.code = request_line.split(" ")[1]
 
-    def get_headers(self,data):
+        return self.code
+
+    def get_headers(self, data):
         return None
 
     def get_body(self, data):
-        return None
-    
+        self.body = data.split("\r\n\r\n")[1]
+
+        return self.body
+
+    def get_path(self, url_obj):
+        if url_obj.path:
+            return url_obj.path
+        else:
+            return "/"
+
+    def get_port(self, url_obj):
+        port = 80
+
+        if url_obj.port:
+            port = url_obj.port
+        elif url_obj.scheme:
+            if url_obj.scheme == 'http':
+                port = 80
+            elif url_obj.scheme == 'https':
+                port = 443
+
+        return port
+
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
-        
+
     def close(self):
         self.socket.close()
 
@@ -68,8 +95,31 @@ class HTTPClient(object):
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+
+        url_obj = urllib.parse.urlparse(url)
+        path = self.get_path(url_obj)
+        port = self.get_port(url_obj)
+        host = url_obj.netloc.split(":")[0]
+        self.connect(host, port)
+
+        reqeust_data = []
+        reqeust_data.append("GET {} HTTP/1.1".format(path))
+        reqeust_data.append("Host: {}".format(host))
+        reqeust_data.append("Accept:  */*")
+        reqeust_data.append("Connection: close")
+        reqeust_data.append("\r\n")
+
+        reqeust_header = "\r\n".join(reqeust_data)
+        print(reqeust_header)
+        self.sendall(reqeust_header)
+
+        response_data = self.recvall(self.socket)
+        code = self.get_code(response_data)
+        body = self.get_body(response_data)
+        print(body)
+
+        self.close()
+
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
@@ -79,10 +129,11 @@ class HTTPClient(object):
 
     def command(self, url, command="GET", args=None):
         if (command == "POST"):
-            return self.POST( url, args )
+            return self.POST(url, args)
         else:
-            return self.GET( url, args )
-    
+            return self.GET(url, args)
+
+
 if __name__ == "__main__":
     client = HTTPClient()
     command = "GET"
@@ -90,6 +141,6 @@ if __name__ == "__main__":
         help()
         sys.exit(1)
     elif (len(sys.argv) == 3):
-        print(client.command( sys.argv[2], sys.argv[1] ))
+        print(client.command(sys.argv[2], sys.argv[1]))
     else:
-        print(client.command( sys.argv[1] ))
+        print(client.command(sys.argv[1]))
